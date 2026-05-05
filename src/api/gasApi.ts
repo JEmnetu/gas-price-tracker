5748ee43ee63d615abf02ed0778a25d5744c7d94
Jacob Habtemariam <jacob@mac.myfiosgateway.com>
2026-05-05 17:14:00 -0400

    add API layer with EIA data parsing and shaping

---
+++ src/api/gasApi.ts
@@ -0,0 +1,8 @@
+import {
+  EIADataPoint,
+  GasPriceDataPoint,
+  GasPriceResponse,
+  NationalTrendData,
+  PADDRegion,
+} from "../types";
+import { PADD_REGIONS, NATIONAL_ID } from "../constants/paddRegions";
